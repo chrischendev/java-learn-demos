d7f48f1febfb2adfe1ae558d4ed6abdb7130f395
chris <chrischen2018@163.com>
2019-07-15 17:48:31 +0800

    mod

--- mahjong-algorithm/src/main/java/com/chris/mahjong/MahjongUtils.java
+++ mahjong-algorithm/src/main/java/com/chris/mahjong/MahjongUtils.java
@@ -1,5 +1,7 @@
 package com.chris.mahjong;
 
+import java.util.List;
+
 /**
  * @author chrischan
  * create on 2019\7\15 0015 17:37
@@ -7,13 +9,23 @@ package com.chris.mahjong;
  */
 public class MahjongUtils {
     /**
-     * 胡牌算法
+     * 胡牌算法 14张
      *
-     * @param ownCard
+     * @param ownCard 已经加上测试可用胡牌的牌
      * @return
      */
     public static boolean hu(OwnCard ownCard) {
+
+        List<Integer> chi = ownCard.getChi();
+        List<Integer> pen = ownCard.getPen();
+        List<Integer> gang = ownCard.getGang();
+        List<Integer> hand = ownCard.getHand();
+        int handSize = hand.size();
+
         //1. 检查手牌张数
+        if (handSize<14){
+            //不够14张 
+        }
 
         return false;
     }
